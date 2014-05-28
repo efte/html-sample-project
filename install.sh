6407f8c962b02a7dc9524edf69c5f100d8bbfb00
kaelzhang <i@kael.me>
2014-05-28 22:21:47 +0800

    adds README.md and install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# create a new profile of settings for this project
+cortex profile add hybrid
+# use it
+cortex profile use hybrid
+
+# set server_mode to `false`, so that you could develop without `cortex server`
+cortex config set server_mode false
+cortex install
+cortex build
+
+# open index.html in your browser
+open ./neurons/cortex-hybrid-sample/0.1.0/template/index.html
